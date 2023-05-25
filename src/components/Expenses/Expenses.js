902ab7d55ffe7951a7f45da0a44a9c2cc2ee6eb8
Boulares Bilel <bilel.boulares@gmail.com>
2023-05-25 22:00:29 +0100

    [BBO] Chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,10 @@
 import React, {useState} from "react";
 
 import './Expenses.css'
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -17,6 +17,7 @@ const Expenses = (props) => {
     return <div>
         <Card className="expenses">
             <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
+            <ExpensesChart expenses={filteredExpenses} />
             <ExpensesList items={filteredExpenses} />
         </Card>
     </div>
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -47,7 +47,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: userInput.enteredTitle,
-            amount: userInput.enteredAmount,
+            amount: +userInput.enteredAmount,
             date: new Date(userInput.enteredDate),
         }
         props.onSaveExpenseData(expenseData);
