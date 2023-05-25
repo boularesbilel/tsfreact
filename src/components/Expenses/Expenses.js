9354a9edb5e485c154c4f7a5badc016d4a88426d
Boulares Bilel <bilel.boulares@gmail.com>
2023-05-25 12:47:42 +0100

    [BBO] Dynamic array

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+
+import './Expenses.css'
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+
+const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
+    return <div>
+        <Card className="expenses">
+            <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
+            {props.items.map(item => <ExpenseItem key={item.id} date={item.date} title={item.title} amount={item.amount}/>)}
+        </Card>
+    </div>
+}
+
+export default Expenses;
